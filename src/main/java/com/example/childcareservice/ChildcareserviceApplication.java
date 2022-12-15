b857fb3cfd804729b8b168a961faae3adea73c54
aasin <tkdghks729@gmail.com>
2022-12-16 00:47:49 +0900

    init

---
+++ src/main/java/com/example/childcareservice/ChildcareserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.childcareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildcareserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildcareserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/childcareservice/ChildcareserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childcareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildcareserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
