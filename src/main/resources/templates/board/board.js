707159c7ce0d306837c091e3a24741bf8b21531c
YooHeeJoon <yhjyhj3855@gmail.com>
2022-12-18 17:56:35 +0900

    별점 기능 starRating

--- src/main/resources/templates/board/board.js
+++
@@ -1,8 +0,0 @@
-const getStar = (i) => {
-    $("#star_rating").text(`별점 : ${5 - i}`)
-}
-for (let i = 0; i < 5; i++) {
-    $("#star")
-        .append(`<input type="radio" id="star${i}" name="star" onclick="getStar(${i})">
-                    <label for="star${i}"></label>`)
-}
