7e7c40260c5b775dc23c6329a6ff02a697780f9e
hadyn <durtlehdduq@gmail.com>
2023-01-02 01:00:10 +0900

    로그인 로그아웃 작성

--- src/main/java/com/example/childcareservice/controller/MainPageController.java
+++ src/main/java/com/example/childcareservice/controller/MainPageController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Slf4j
@@ -14,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainPageController {
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String mainPage(){
-        return "babysitter";
+        return "index";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "index";
     }
 }
