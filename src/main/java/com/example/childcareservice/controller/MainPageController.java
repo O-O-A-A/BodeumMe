76f60f8abda3f1caa7221de6860bb008f1b7e537
hadyn <durtlehdduq@gmail.com>
2023-01-03 00:05:08 +0900

    카카오 oauth 구현

--- src/main/java/com/example/childcareservice/controller/MainPageController.java
+++ src/main/java/com/example/childcareservice/controller/MainPageController.java
@@ -1,10 +1,9 @@
 package com.example.childcareservice.controller;
 
+import com.example.childcareservice.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class MainPageController {
+    private final KakaoService kakaoService;
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String mainPage(){
         return "index";
@@ -22,7 +22,14 @@ public class MainPageController {
 
     @RequestMapping("/logout")
     public String logout(HttpServletRequest request){
+        if(request.getSession().getAttribute("token") != null){
+            kakaoService.logoutKakao((String)request.getSession().getAttribute("token"));
+            log.info("kakao logout");
+        }
         request.getSession().invalidate();
         return "index";
     }
+
+
+
 }
